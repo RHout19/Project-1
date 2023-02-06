b944e0407ce1ebe1bfa8aa537034552de753f07b
RHout19 <renevanderhout@gmail.com>
2023-02-06 11:35:51 +0100

    First lines of code

---
+++ Exercise_Basics.py
@@ -0,0 +1,25 @@
+#Exercise 1
+name="Rene"
+print(name)
+
+#Exercise 2
+age=29
+print(age)
+
+#Exercise3
+julia_age=32
+age_diff=julia_age-age
+print(age_diff)
+
+#Exercise 4
+print("You are older than 21") if age > 21 else print("You are not older than 21")
+
+#Exercise 5
+if age > julia_age:
+  print("Julia is younger than you")
+elif age == julia_age:
+  print("You have the same age as Julia")
+else:
+  print("Julia is older than you")
+
+#Exercise 6
---
+++ Exercise_List&Loops.py
@@ -0,0 +1,8 @@
+#Exercise 6
+to_do_list = ['meet Anna', 'buy milk', 'finish latest blog post']
+print(to_do_list[1])
+
+year_list = [1999, 1995, 2005, 2010, 2007, 2006, 1994, 1996, 1979, 2008]
+print(year_list)
+curret_year = 2023
+print(curret_year - year_list[0])
